05aad2e0c854e8ef06e0d3882e46bb5a4aa457b9
yoshimaputri <yoshimashima@gmail.com>
2022-10-06 14:36:51 +0900

    init service worker

---
+++ src/custom-service-worker.js
@@ -0,0 +1,9 @@
+// workbox.routing.registerRoute(
+//     new RegExp("/path/to/cache/directory/"),
+//     workbox.strategies.NetworkFirst()
+// );
+// workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
+
+// self.addEventListener('fetch', e => {
+//     console.log('fetch event', e.request.method, e.request.url);
+// });
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+module.exports = function override(config, env) {
+    config.plugins = config.plugins.map((plugin) => {
+        if (plugin.constructor.name === "GenerateSW") {
+            return new WorkboxWebpackPlugin.InjectManifest({
+                swSrc: "./src/custom-service-worker.js",
+                swDest: "service-worker.js"
+            });
+        }
+        return plugin;
+    });
+    return config;
+};
